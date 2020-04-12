fada146235e855abe25c27d271d08960db045549
Ting <wisezt@hotmail.com>
2020-04-12 16:52:06 -0400

    Add form and post method

--- index.js
+++ index.js
@@ -4,10 +4,33 @@ const express = require('express');
 
 const app = express();
 
-
+// the first parameter of app.get() is the router handler, here it is the '/'
 app.get('/', (req, res)=>{
-    res.send('<h1>hi there<h1>');
+    res.send(`
+    <h1>Admin Panel</h1>
+    <div>
+        <p>Sign Up</p>
+        <form method="POST" action="/">
+            <input placeholder="email" />
+            <br><br>
+            <input placeholder="password" />
+            <br><br>
+            <input placeholder="password confirmation" />
+            <br><br>
+            <button>Sign Up</button>
+        </form>
+    </div>
+    `);
 });
+
+
+app.post('/', (req, res)=>{
+
+    res.send('account created');
+
+
+});
+
 
 
 app.listen(3000, ()=>{
