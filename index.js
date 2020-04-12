feb38ba475a80906a19d3cd1ce7a1f029ba3a602
Ting <wisezt@hotmail.com>
2020-04-12 17:25:48 -0400

    fixed no response from req.on() by adding name property in input tag

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("This one WOrks");
+console.log("This one Works");
 
 const express = require('express');
 
@@ -10,12 +10,12 @@ app.get('/', (req, res)=>{
     <h1>Admin Panel</h1>
     <div>
         <p>Sign Up</p>
-        <form method="POST" action="/">
-            <input placeholder="email" />
+        <form method="POST" >
+            <input name="email" placeholder="email" />
             <br><br>
-            <input placeholder="password" />
+            <input name="password" placeholder="password" />
             <br><br>
-            <input placeholder="password confirmation" />
+            <input name="passwordConfiramtion" placeholder="password confirmation" />
             <br><br>
             <button>Sign Up</button>
         </form>
@@ -25,7 +25,13 @@ app.get('/', (req, res)=>{
 
 
 app.post('/', (req, res)=>{
+    // get access to the information send by the browser.
+    req.on('data', data =>{
+        console.log(data.toString('utf8'));
+    });
 
+    console.log("Get Something");
+    
     res.send('account created');
 
 
