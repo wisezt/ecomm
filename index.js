be9b105523fbd3c5f255bfaf1ff3de7dbed4abed
Ting <wisezt@hotmail.com>
2020-04-12 20:18:46 -0400

    Add body-Parser library

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 console.log("This one Works");
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 // the first parameter of app.get() is the router handler, here it is the '/'
 app.get('/', (req, res)=>{
@@ -26,28 +30,28 @@ app.get('/', (req, res)=>{
 
 // where is the next function come from?
 // My guest, it is building by the app.post() itself.
-const bodyParser = (req, res, next) =>{
-    if (req.method === 'POST'){
-        req.on('data', data=>{
-            const parsed = data.toString('utf8').split('&');
-            const formDate = {};
-            for (let pair of parsed){
-                const [key, value] = pair.split('=');
-                formDate[key] = value;
-            }
-            req.body = formDate;
-            next();
-        }
-        );
-    } else {
-        next();
-    }
-}
+// const bodyParser = (req, res, next) =>{
+//     if (req.method === 'POST'){
+//         req.on('data', data=>{
+//             const parsed = data.toString('utf8').split('&');
+//             const formDate = {};
+//             for (let pair of parsed){
+//                 const [key, value] = pair.split('=');
+//                 formDate[key] = value;
+//             }
+//             req.body = formDate;
+//             next();
+//         }
+//         );
+//     } else {
+//         next();
+//     }
+// }
 
 
 
 
-app.post('/', bodyParser ,(req, res)=>{
+app.post('/', (req, res)=>{
     // get access to the information send by the browser.
     console.log(req.body);
     
@@ -55,6 +59,18 @@ app.post('/', bodyParser ,(req, res)=>{
 
 
 });
+
+
+
+app.post('/products',  (req, res)=>{
+    // get access to the information send by the browser.
+    console.log(req.body);
+    
+    res.send('account created');
+
+
+});
+
 
 
 
