26b8db0705b17438fb2334ce307c69cf54320c1e
Ting <wisezt@hotmail.com>
2020-04-18 12:57:55 -0400

    Add find/delete functions in user.js

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository{
     constructor(filename){
@@ -27,15 +28,59 @@ class UsersRepository{
 
         // Return the parsed date
         return data;
+
     }
+
+    async create (attrs){
+        attrs.id = this.randomId();
+
+        const records = await this.getAll();
+        records.push(attrs);
+
+        await this.writeAll(records);
+
+        // await fs.promises.writeFile(this.filename, JSON.stringify(records));
+
+    }
+
+
+    async writeAll(records){
+        await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+    }
+
+    randomId(){
+        return crypto.randomBytes(4).toString('hex');
+    }
+
+    async getOne(id){
+        const records = await this.getAll();
+        return records.find(record => record.id === id );
+    }
+
+    async delete(id){
+        const records = await this.getAll();
+        const filteredRecords = records.filter(record => record.id !== id);
+
+        // is this really the best way?
+        // Read all the records from Database then rewrite all the records which not match the id, back to the Database.
+        // So this means we have to read and write all data everytime we delete a record.
+        await this.writeAll(filteredRecords);
+    }
+
+
 }
 
 
 const test = async () => {
     const repo = new UsersRepository('users.json');
+    
+    // const user = await repo.getOne('ac27d176');
+
+    await repo.delete("e33ae98d");
+
     const users = await repo.getAll();
+
     console.log(users);
 }
 
 test();
-new UsersRepository('users.json');
